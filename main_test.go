ba0853844994070385514bccfbaa52f25f0a43c8
Yegor Maksymchuk <ymaksymc@redhat.com>
2019-01-31 14:34:03 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ func TestGenerateQuestions(t *testing.T) {
 	expectedResult := 2
 	actualResult := len(generateQuestions(2))
 	if expectedResult != actualResult {
-		t.Error("Exception actual result is 2 but got a ", actualResult)
+		t.Error("Exception expected result is 2 but got a ", actualResult)
 	}
 
 }
